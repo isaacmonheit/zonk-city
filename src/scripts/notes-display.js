7191adf87151b2b8b572ce14b1636f92973e6b95
isaacmonheit <isaacmonheit@gmail.com>
2024-08-12 22:42:12 -0600

    add windows

---
+++ src/scripts/notes-display.js
@@ -0,0 +1,9 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const content = document.getElementById('toggle-content');
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+    } else {
+        content.style.display = 'none';
+    }
+});
+
