ac5086d8ec7a3f6669939ede405874aad27edb59
isaacmonheit <isaacmonheit@gmail.com>
2024-08-14 23:39:40 -0600

    icons

--- src/scripts/notes-display.js
+++ src/scripts/notes-display.js
@@ -1,9 +1,11 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-    const content = document.getElementById('toggle-content');
-    if (content.style.display === 'none' || content.style.display === '') {
-        content.style.display = 'block';
-    } else {
-        content.style.display = 'none';
-    }
-});
-
+const hamburger = document.getElementById('hamburger')
+if (hamburger) {
+    hamburger.addEventListener('click', function() {
+        const content = document.getElementById('toggle-content');
+        if (content.style.display === 'none' || content.style.display === '') {
+            content.style.display = 'block';
+        } else {
+            content.style.display = 'none';
+        }
+    });
+}
